1767d13ea2ec75dbbb954cbabab87549bedad0d7
Cole Hunter <cole.joshua.hunter@outlook.com>
2020-10-09 18:22:30 -0500

    updated database.py to seed demo user with flashcards

--- Cram/starter_app/database.py
+++ Cram/starter_app/database.py
@@ -15,7 +15,7 @@ with app.app_context():
   demo_user_new_deck2 = Deck(title='Javascript', isMastered=False, userId=1)
   demo_user_flashcard_2 = Flashcard(question='What is Javascript?', answer="An object-oriented computer programming language commonly used to create interactive effects within web browsers.", confidence=0, userId=1, deckId=2)
   demo_user_new_deck3 = Deck(title='Docker', isMastered=False, userId=1)
-  demo_user_flashcard = Flashcard(question='What is Docker?', answer="A set of products that use OS-level virtualization to deliver software in packages called containers.", confidence=0, userId=1, deckId=3)
+  demo_user_flashcard_3 = Flashcard(question='What is Docker?', answer="A set of products that use OS-level virtualization to deliver software in packages called containers.", confidence=0, userId=1, deckId=3)
 
 
 
